e1ed9fbcc2dbfd80e39271da6cb475aff4a03781
Hemanth Madu <173272876+hemu-madu@users.noreply.github.com>
2026-01-08 11:52:38 +0530

    Add Day-1 programs

---
+++ DAY-1/LCM.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
+
+int main()
+{
+    int a = 12, b = 18;
+    printf("LCM = %d", lcm(a, b));
+    return 0;
+}
